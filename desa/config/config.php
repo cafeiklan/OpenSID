105aaa6b618e0433af7cc4a050c7cae245a2057f
cafeiklan <info@cafeiklan.com>
2017-02-22 19:59:17 +0700

    Add config desa

---
+++ desa/config/config.php
@@ -0,0 +1,24 @@
+<?php
+// -------------------------------------------------------------------------
+// Konfigurasi applikasi dalam file ini merupakan item konfigurasi tambahan
+// SID di donjo-app/config/config.php.
+// -------------------------------------------------------------------------
+
+// key dari layanan google map
+$config['google_key'] = 'ganti-dengan-google-api-key';
+
+
+// key dari layanan google map
+$config['google_key'] = 'ganti-dengan-google-api-key';
+$config['libreoffice_path'] = "C:\Program Files (x86)\LibreOffice 4\program";
+
+// penyesuaian judul yang muncul di browser
+$config['website_title'] = 'Website Resmi Desa Kotapari';
+$config['login_title'] = 'Desa Kotapari';
+$config['admin_title'] = 'Sistem Informasi Desa Kotapari';
+
+// untuk mengganti penamaan wilayah 'desa'
+$config['sebutan_desa'] = 'desa';
+$config['sebutan_dusun'] = 'dusun';
+
+$config['offline_mode'] = TRUE;
