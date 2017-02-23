ce0de4802d12a132061d7898846a249067be2a52
cafeiklan <info@cafeiklan.com>
2017-02-23 09:40:11 +0700

    Tambahkan opsi enable/disable track ke server sid

--- donjo-app/models/track_model.php
+++ donjo-app/models/track_model.php
@@ -2,6 +2,15 @@
 
   function __construct(){
     parent::__construct();
+    
+    $this->session->set_userdata('enable_track', TRUE);
+    
+    if($this->config->item('enable_track') == FALSE )
+    	$this->session->set_userdata('enable_track', FALSE);
+    
+    if($this->config->item('offline_mode') == TRUE)
+    	$this->session->set_userdata('enable_track', FALSE);
+    
   }
 
   function track_desa($dari){
